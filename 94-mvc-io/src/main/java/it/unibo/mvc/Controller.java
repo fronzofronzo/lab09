076c823959f45fe4338f27727eab39a3bac630a9
fronzofronzo <146710462+fronzofronzo@users.noreply.github.com>
2023-11-26 10:02:58 +0100

    Implemented Controller Interface and SimpleController class

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,34 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * A Controller responsible for I/O access. It consider only the standard output
  */
 public interface Controller {
 
+    /**
+     * This method gets the next string to be printed on standard output
+     */
+    public String getNextString();
+
+    /**
+     * Set to the next String to be printed 
+     * @param str string got by input
+     * @throws IllegalArgumentException the string that will be passed in input 
+     * should not be null
+     */
+    public void setNextString(String str);
+
+    /**
+     * This method get the history of printed strings
+     * @return a List with all the Strings that had been printed
+     */
+    public List<String> getHistory();
+
+    /**
+     * This method prints the current string that has been set
+     * @throws IllegalStateException if the current string is unset
+     */
+    public void printCurrentString();
 }
--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,46 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 
+ * This class implements a SimpleController. In toBePrinted is stored 
  *
  */
 public final class SimpleController implements Controller {
+
+    private String toBePrinted = null;
+    private List<String> history;
+
+    public SimpleController() {
+        history = new ArrayList<>();
+    }
+
+    @Override
+    public String getNextString() {
+        return toBePrinted;
+    }
+
+    @Override
+    public void setNextString(String str) {
+        if(str == null) {
+            throw new IllegalArgumentException("String should not be null");
+        }
+        toBePrinted = str;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return new ArrayList<>(List.copyOf(history));
+    }
+
+    @Override
+    public void printCurrentString() {
+        if(this.toBePrinted == null) {
+            throw new IllegalStateException("String is unset");
+        }
+        System.out.println(toBePrinted);
+        history.add(toBePrinted);
+    }
 
 }
